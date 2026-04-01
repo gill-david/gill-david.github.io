4f6c345d6ee6132b97bf501943ef664ce3fcd96d
gill-david <dgill.econ@gmail.com>
2026-04-01 14:52:22 -0400

    Add files via upload

---
+++ toggleA.js
@@ -0,0 +1,12 @@
+function resettoggle(){
+    var e = document.getElementById('foo');
+    e.style.display = 'none';
+}
+
+function toggle_visibility(id){
+    var e = document.getElementById(id);
+    if (e.style.display == 'none') 
+        e.style.display = 'block';
+    else 
+        e.style.display = 'none';
+}
